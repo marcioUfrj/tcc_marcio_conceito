c1d02f408aba0fd0660b9ee8cb6fcf9d4c2d2621
marcioUfrj <marcioteixeira23@poli.ufrj.br>
2022-04-18 10:10:50 -0300

    backend adjust

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.json('index page')
+  res.send('index page')
 })
 
 module.exports = router
